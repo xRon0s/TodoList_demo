eab68392bb6fc71c96d4b03d7632e4b548099dd5
Reloaded <137490689+xRon0s@users.noreply.github.com>
2025-10-31 10:15:48 +0900

    repository_miss

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const repositoryName = "react-todo-app";
+const repositoryName = "TodoList_demo";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-const repositoryName = "react-todo-app";
+const repositoryName = "TodoList_demo";
 
 // https://vite.dev/config/
 export default defineConfig({
