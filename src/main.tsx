f4ac7d9eb2df0fd54a634d8b86813e1a4f8dd9ad
Reloaded <137490689+xRon0s@users.noreply.github.com>
2025-10-31 10:12:27 +0900

    and err

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const repositoryName = "react-todo-app";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter
+      basename={import.meta.env.PROD ? `/${repositoryName}/` : "/"}
+    >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
